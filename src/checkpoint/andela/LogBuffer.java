f3db90745cb122b58dd3f71a3b38e3feb2ab0c22
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-27 18:58:53 +0100

    Add LogBuffer class

---
+++ src/checkpoint/andela/LogBuffer.java
@@ -0,0 +1,23 @@
+package checkpoint.andela;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class LogBuffer implements Buffer {
+    private ArrayBlockingQueue<String> buffer;
+
+    public LogBuffer() {
+        buffer = new ArrayBlockingQueue<>(1);
+    }
+
+    @Override
+    public void set(String line) throws InterruptedException {
+        buffer.put(line);
+
+    }
+
+    @Override
+    public String get() throws InterruptedException {
+        return buffer.poll();
+    }
+
+}
