1907bb5f462208b9022b35196320a64898421729
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-27 19:05:29 +0100

    Add Constants class

---
+++ src/checkpoint/andela/Constants.java
@@ -0,0 +1,10 @@
+package checkpoint.andela;
+
+public class Constants {
+    public final static String DATABASE_URL = "jdbc:mysql://localhost:3306/new_schema1";
+    public final static String USER = "root";
+    public final static String PASSWORD = "";
+    public final static String TABLE = "react";
+    public final static String DATA_FILENAME = "files/reactions.dat";
+    public final static String LOG_FILENAME = "files/reactions.txt";
+}
