5838e6745d24190806260ef97031849c1ff07c50
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-27 19:06:43 +0100

    Add Date class

---
+++ src/checkpoint/andela/Date.java
@@ -0,0 +1,12 @@
+package checkpoint.andela;
+
+import java.text.SimpleDateFormat;
+
+public class Date {
+
+    public String getDate() {
+        java.util.Date date = new java.util.Date();
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sd.format(date);
+    }
+}
