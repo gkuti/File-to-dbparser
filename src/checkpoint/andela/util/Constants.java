9e3607f30ad9ce714436b72325074447ccf8abb4
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-29 16:55:52 +0100

    Update FileParser and Constants classes

--- src/checkpoint/andela/util/Constants.java
+++ src/checkpoint/andela/util/Constants.java
@@ -4,10 +4,10 @@ package checkpoint.andela.util;
  * Constants class
  */
 public class Constants {
-    public final static String DATABASE_URL = "jdbc:mysql://localhost:3306/new_schema1";
+    public final static String DATABASE_URL = "jdbc:mysql://localhost:3306/reactiondb";
     public final static String USER = "root";
     public final static String PASSWORD = "";
-    public final static String TABLE = "react";
+    public final static String TABLE = "reactions";
     public final static String DATA_FILENAME = "files/reactions.dat";
     public final static String LOG_FILENAME = "files/reactions.txt";
 }
