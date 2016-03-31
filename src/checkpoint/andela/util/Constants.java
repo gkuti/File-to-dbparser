1cc526bb0d5f3212743b7c87a26c36cde4045435
andela-gkuti <kuti.gbolahan@andela.com>
2016-03-31 09:43:20 +0100

    Add constructor and getValue method to Constants enum

--- src/checkpoint/andela/util/Constants.java
+++ src/checkpoint/andela/util/Constants.java
@@ -4,5 +4,21 @@ package checkpoint.andela.util;
  * Constants class
  */
 public enum Constants {
-    
+
+    DATABASE_URL("jdbc:mysql://localhost:3306/reactiondb"),
+    USER("root"),
+    PASSWORD(""),
+    TABLE("reactions"),
+    DATA_FILENAME("files/reactions.dat"),
+    LOG_FILENAME("files/reactions.txt");
+
+    String value;
+
+    Constants(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
